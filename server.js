ad0c9e7bf554062a67255ea0c0cccc67f78a4426
louteos <gdismw@gmail.com>
2023-02-21 12:05:04 +0900

    서버연동 시작

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express'); // 서버기능 구현을 위한 코어모듈 express 호출
-const path = require('path'); // 경로기능 제어하기 위해 모듈 호출
-const cors = require('cors'); // node 서버 이외 다른 서버가 존재함을 알 수 있음
-const app = express(); // express 실행
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8002;
 
-app.use(cors()); // 다른 도메인의 명령 수용
+app.use(cors());
 
-app.get('/', (req,res) => { // get, post, use(서버 자체에서 라우터 만들 때)
-  res.send({message:'hello'});
-});
+app.use(express.static( path.join(__dirname, 'public')))
 
-app.listen(8000, ()=>{ // listen: 실제 실행문
-  console.log('server is running on 8000')
+app.get('/', (req,res) =>
+  res.sendFile( path.join(__dirname, 'public/index.html'))
+);
+
+app.use((req, res) =>{
+  res.status(404).sendFile( path.join(__dirname, 'public/nopage.html'))
+})
+
+app.listen(PORT, () => {
+  console.log( `${PORT} 노드서버구동정상` )
 })
