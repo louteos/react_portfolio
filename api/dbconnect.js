67e9cfbd85ff32a4fc3f915f92f87c5d4b5a3784
louteos <gdismw@gmail.com>
2023-02-24 14:13:32 +0900

    카페24 노드확인전

--- api/dbconnect.js
+++ api/dbconnect.js
@@ -7,40 +7,30 @@ const conn = mysql.createPool(dbconfig);
 router.use(express.urlencoded({extended: false}));
 
 
-// var mybatisMapper = require('mybatis-mapper');
-// mybatisMapper.createMapper(['./mapper/reactSQL.xml']);
-
-var format = { language : 'sql', indent : '  '}
-
 
 router.post('/', (req, res) => {
    
     var params = req.body;
 
-    var listQuery = ` insert into
+    var contactQuery = ` insert into
                         lout_contact_table ( nm, company, mail, msg )
-                      values ( nm, company, mail, msg )
+                        values ( ?, ?, ?, ? )
                     `
 
-     console.log(params)
+     console.log(params);
 
 
-    // conn.getConnection( ( err, connection ) => {
-    //   if(err) throw console.log(" 이 에러가 보인다면 dB정보 틀린거임  : " + err);
+    conn.getConnection( ( err, connection ) => {
+      if(err) throw console.log(" 이 에러가 보인다면 dB정보 틀린거임  : " + err);
 
-    //   connection.query(listQuery, (error, result) => {
-       
-    //     if(error) throw "여기 에러는 sql문 오류"+ error + result; // result를 받지 못하는 상황
-    //     res.send('성공' -> insert 된것); 
+      connection.query(contactQuery, (error, result) => {
+        if(error) throw "여기 에러는 sql문 오류"+ error + result;
+        res.send('성공'); 
 
-    //   })
-    //   connection.release(); // 위의 연결 끝내기 그래야 다음 서버 접속자 대기에서 사용으로
+      })
+      connection.release();
 
-    // })
-
-    
-    
-        
+    })
 })
 
 
--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 8002;
 const apiurl = require('./api/dbconnect')
 
-app.use(cors());
 app.use('/api',apiurl )
 
 app.use(express.static( path.join(__dirname, 'public')))
